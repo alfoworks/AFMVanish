2a9629016da294a4319d337cd988f5a7df4a87ec
HeroBrine1st Erquilenne <pankov.76.tvink@gmail.com>
2019-11-19 17:20:59 +0300

    Ебнули классы

---
+++ src/main/java/ru/allformine/afmvanish/commands/VanishNoInteractCommand.java
@@ -0,0 +1,36 @@
+package ru.allformine.afmcp.commands;
+
+import org.spongepowered.api.command.CommandException;
+import org.spongepowered.api.command.CommandResult;
+import org.spongepowered.api.command.CommandSource;
+import org.spongepowered.api.command.args.CommandContext;
+import org.spongepowered.api.entity.living.player.Player;
+import org.spongepowered.api.text.Text;
+import org.spongepowered.api.text.format.TextColors;
+import ru.allformine.afmcp.vanish.VanishManager;
+
+public class VanishNoInteractCommand extends AFMCPCommand {
+    @Override
+    public CommandResult execute(CommandSource src, CommandContext args) throws CommandException {
+        if(src instanceof Player){
+            if(!VanishManager.isVanished((Player) src)){
+                reply(src, Text.of("You must be vanished"));
+            }
+            boolean state = VanishManager.switchCanInteract((Player) src);
+            Text.Builder text = Text.builder();
+            text.append(Text.of("Теперь вы "));
+            if (state) text.append(Text.of("можете"));
+            else text.append(Text.builder().append(Text.of("не можете")).color(TextColors.RED).build());
+            text.append(Text.of(" взаимодействовать с миром."));
+            reply(src, text.build());
+        }else{
+            reply(src, Text.of("Еблан, от консоли нельзя"));
+        }
+        return CommandResult.success();
+    }
+
+    @Override
+    public String getName() {
+        return "Vanish";
+    }
+}
