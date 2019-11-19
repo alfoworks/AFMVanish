7c2b10a986061e8967530e277b154be26db2dc44
HeroBrine1st Erquilenne <pankov.76.tvink@gmail.com>
2019-11-19 17:12:13 +0300

    Заливаем весь проект

---
+++ src/main/java/ru/allformine/afmvanish/AFMVanish.java
@@ -0,0 +1,26 @@
+package ru.allformine.afmvanish;
+
+import com.google.inject.Inject;
+import org.slf4j.Logger;
+import org.spongepowered.api.event.game.state.GameStartedServerEvent;
+import org.spongepowered.api.event.Listener;
+import org.spongepowered.api.plugin.Plugin;
+
+@Plugin(
+        id = "afmvanish",
+        name = "AFMVanish",
+        description = "AFMVansh",
+        authors = {
+                "IteratorW",
+                "HeroBrine1st Erquilenne"
+        }
+)
+public class AFMVanish {
+
+    @Inject
+    private Logger logger;
+
+    @Listener
+    public void onServerStart(GameStartedServerEvent event) {
+    }
+}
